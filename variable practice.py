bd465ba08b5794f234f1e38354615afd981c5315
Varshnaiy <77457051+varshnaiy007@users.noreply.github.com>
2025-06-05 01:57:28 +0530

    Create variable practice.py

---
+++ variable practice.py
@@ -0,0 +1,30 @@
+Variable Practising::
+---------------------
+Example: 1. Shoping Cart
+            ------------
+item = "polo"
+quantity = 2
+price = 499
+total_price = price * quantity
+print("Total Cost is:", total_cost)
+
+O/P : Total price is: 998
+
+
+Example: 2. Flight Ticket
+            ------------
+logic: person_name; ticket price; 
+       souce : bandra terminal destination:  Jammu.
+     
+person_name = "Varro"
+source = "Bandra Terminal"
+destination = "Jammu"
+ticket_price = 8673
+print(peson_name "is travelling from" source "to" destination)
+print("Cost of his flight is:", ticket_price)
+
+O/P : Varro is travelling from Bandra Terminal to Jammu
+      Cost of his flight is: 8673
+
+
+
