ca7c7ca9a8eadbb66d0c23e9b27099e13110b4f9
Varshnaiy <77457051+varshnaiy007@users.noreply.github.com>
2025-06-05 03:49:59 +0530

    Create WAP Evaluate Car's Mileage & student  marks.py

---
+++ WAP Evaluate Car's Mileage & student  marks.py
@@ -0,0 +1,21 @@
+WAP Evaluate Car's Mileage
+Exercise 3: Evaluate Car's Mileage
+            ----------------------
+
+car_name = "Skoda Rapid"
+model_year = 2018
+mileage = 20
+print(car_name, model_year, "year car is having" mileage, "KMPL")
+
+O/P : Skoda Rapid 2018 year car is having 20 KMPL
+
+
+Exercise 4: Student name, class and marks stored.
+            -------------------------------------
+
+student_name = "Parul"
+_class = "8TH class"
+Marks = "90%"
+print(student_name, "is from",_class, "and her marks is:", Marks)
+
+O/P : Parul is from 8TH class and her marks is: 90%
